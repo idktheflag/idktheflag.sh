0a25fcf4a066c7fe7422985b1a01ae02e96e0f7b
scrappy backend <backend@qu1ck5h0t_is_goated.com>
2026-07-28 01:58:16 +0000

    skibidi slop sahurrr

--- src/data/events.ts
+++ src/data/events.ts
@@ -33,4 +33,5 @@ export const events: CtfEvent[] = [
   { name: 'BroncoCTF 2026', date: '2026-07-11', place: 8, points: 3777, rating: 19.586 },
   { name: 'OmniCTF 2026 Quals', date: '2026-07-17', place: 21, points: 1916, rating: 17.484 },
   { name: 'ENOWARS 10', date: '2026-07-18', place: 76, points: 22450.9147, rating: 40.366 },
+  { name: 'jailCTF 2026', date: '2026-07-24', place: 15, points: 154139, rating: 3.606 },
 ];
--- src/data/stats.ts
+++ src/data/stats.ts
@@ -2,5 +2,5 @@ export const teamStats = {
   globalRank: 115,
   countryRank: 12,
   ratingPoints: 319.672,
-  eventsPlayed: 26,
+  eventsPlayed: 27,
 };
